d230d3239c3b51f386ce9727dfcdb07732d04e42
정삼복 <68502584+3boku@users.noreply.github.com>
2024-02-22 13:00:15 +0900

    ip allow

--- back/controllers/root.go
+++ back/controllers/root.go
@@ -5,18 +5,40 @@ import (
 	"back/services"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"time"
 )
 
 func NewController(port string) {
-	r := gin.Default()
+	r := gin.New()
 
-	r.Use(cors.New(
-		cors.Config{
-			AllowOrigins: []string{"http://192.168.219.105"},
-			AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
-			MaxAge:       24 * time.Hour,
-		}))
+	r.Use(gin.Logger())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost", "http://127.0.0.1", "http://112.159.30.237"},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE"},
+		MaxAge:       24 * time.Hour,
+	}))
+
+	// Access Control Middleware
+	r.Use(func(c *gin.Context) {
+		// 내 서버에 접근하는지 확인
+		requestIP := c.ClientIP()
+		// 허용된 IP 리스트
+		allowedIPs := []string{"127.0.0.1", "::1", "localhost", "112.159.30.237"}
+		isAllowed := false
+		for _, ip := range allowedIPs {
+			if requestIP == ip {
+				isAllowed = true
+				break
+			}
+		}
+		if !isAllowed {
+			c.JSON(http.StatusForbidden, gin.H{"error": "접근이 거부되었습니다."})
+			c.Abort()
+			return
+		}
+		c.Next()
+	})
 
 	rdb := repository.MySQLInit()
 	ndb := repository.MongoDBInit()
