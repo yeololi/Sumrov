188be2a3b58b3b761e0526e1255689af8b5a27c1
asdf1596 <109271987+asdf1596@users.noreply.github.com>
2024-03-26 16:39:06 +0900

    review3(dark)

--- next/app/(mobile)/mobile/review3/page.tsx
+++ next/app/(mobile)/mobile/review3/page.tsx
@@ -13,44 +13,44 @@ const Review = () => {
                 </div>
                 <div className="w-full flex justify-center h-[533px]  mb-[45px]">
                     <div className="w-[90%]">
-                        <button className="w-full h-[100px] bg-[#fafafa] border-[1px] border-solid border-[#d3d3d3] p-[13px] flex items-center gap-[13px]">
+                        <button className="w-full h-[100px] bg-[#fafafa] border-[1px] border-solid border-[#d3d3d3] dark:border-[#EBEBEB] p-[13px] flex items-center gap-[13px]">
                             <img
                                 width="74"
                                 height="74"
                                 src="/images/camera.png"
                             ></img>
-                            <div className="w-[78px] h-[25px] bg-[#ffffffa8] border-[1px] border-solid border-[#d3d3d3] items-center text-center">
+                            <div className="w-[78px] h-[25px] bg-[#ffffffa8] text-black border-[1px] border-solid border-[#d3d3d3] items-center text-center">
                                 사진첨부
                             </div>
                         </button>
-                        <div className="w-full h-[50px] p-[11px] border-b border-l border-r flex justify-between items-center whitespace-nowrap">
+                        <div className="w-full h-[50px] p-[11px] border-b border-l border-r dark:border-[#EBEBEB] flex justify-between items-center whitespace-nowrap">
                             <div>제목</div>
                             <div className="flex">
-                                <div className="w-[204px] h-[26px] border border-black border-opacity-5"></div>
-                                <div className="w-[36px] h-[26px] border border-black border-opacity-5 flex items-center justify-center">
+                                <div className="w-[204px] h-[26px] border border-black dark:border-[#EBEBEB] border-opacity-5"></div>
+                                <div className="w-[36px] h-[26px] border border-black dark:border-[#EBEBEB] border-opacity-5 flex items-center justify-center">
                                     <ChevronDown className="w-6 h-6 text-neutral-400" />
                                 </div>
                             </div>
                         </div>
-                        <div className="w-full h-[50px] p-[11px] border-b border-l border-r flex justify-between items-center whitespace-nowrap">
+                        <div className="w-full h-[50px] p-[11px] border-b border-l border-r dark:border-[#EBEBEB] flex justify-between items-center whitespace-nowrap">
                             <div>작성자</div>
                             <div className="flex">
-                                <div className="w-[204px] h-[26px] border border-black border-opacity-5"></div>
-                                <div className="w-[36px] h-[26px] border border-black border-opacity-5 flex items-center justify-center">
+                                <div className="w-[204px] h-[26px] border border-black dark:border-[#EBEBEB] border-opacity-5"></div>
+                                <div className="w-[36px] h-[26px] border border-black dark:border-[#EBEBEB] border-opacity-5 flex items-center justify-center">
                                     <ChevronDown className="w-6 h-6 text-neutral-400" />
                                 </div>
                             </div>
                         </div>
-                        <div className="w-full h-[333px] pt-[11px] border-b border-l border-r flex justify-center">
+                        <div className="w-full h-[333px] pt-[11px] border-b border-l border-r dark:border-[#EBEBEB] flex justify-center">
                             <div className="w-[95%] flex flex-col">
                                 <div className="w-full h-[70%] flex flex-col">
-                                    <div className="w-full h-[85%] border border-black border-opacity-5"></div>
-                                    <div className="w-full h-[15%] border border-black border-opacity-5 text-[11px] text-[#919191] text-end p-[8px]">
+                                    <div className="w-full h-[85%] border border-black dark:border-[#EBEBEB] border-opacity-5"></div>
+                                    <div className="w-full h-[15%] border border-black dark:border-[#EBEBEB] border-opacity-5 text-[11px] text-[#919191] text-end p-[8px]">
                                         문자 : 0
                                     </div>
                                 </div>
                                 <div className="w-full flex mt-[32px] justify-center gap-[12px]">
-                                    <div className="w-[104px] h-[22px] text-white bg-black text-center text-[9px] pt-[4px]">
+                                    <div className="w-[104px] h-[22px] text-white dark:bg-white dark:text-black bg-black text-center text-[9px] pt-[4px]">
                                         등록
                                     </div>
                                     <div className="w-[104px] h-[22px] border-[1px] border-solid border-[#ebebeb] text-center text-[9px] pt-[4px]">
