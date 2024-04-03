77b2a8599f3793d6558390a3d7b5c160db7be623
정삼복 <igiza1213@gmail.com>
2024-04-04 00:18:11 +0900

    test1

--- next/app/api/auth/[...nextauth]/route.ts
+++ next/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     CredentialsProvider({
@@ -65,4 +65,4 @@ export const authOptions: NextAuthOptions = {
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, authOptions };
