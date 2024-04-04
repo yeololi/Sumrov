a4575f072892d372371038cbe4a4755551106ad6
정윤환 <torychu0315@naver.com>
2024-04-04 10:08:19 +0900

    ddd

--- next/app/(desktop)/(admin)/noticePage/page.tsx
+++ next/app/(desktop)/(admin)/noticePage/page.tsx
@@ -144,7 +144,7 @@ const RegistrationPage = () => {
           </div>
         </div>
         <div className="flex-col justify-start items-center gap-[74px] flex">
-          <div className="border border-stone-300 border-t-0 flex-col justify-center items-center flex">
+          <div className="border border-stone-300 border-t-0 flex-col w-[1125px] justify-center items-center flex">
             <div className="flex-col justify-center items-center gap-[10px] flex mt-[48px]">
               <div className="w-[700px] flex-col justify-center items-center gap-[10px] flex">
                 <div className="w-[700px] flex-col  items-center flex">
@@ -227,105 +227,132 @@ const RegistrationPage = () => {
                   </div>
                 </div>
 
-                <div className="text-center text-black text-[22px] font-medium font-noto">
+                <div className="text-center text-black text-[22px] mt-16 font-medium font-noto">
                   미리보기
                 </div>
-
-                <div className="flex-col justify-start items-center gap-[15px] flex">
-                  <div className="justify-center items-center gap-[25px] inline-flex">
-                    <div className="w-[1125px] h-[733px] flex-col justify-center items-center gap-[50px] inline-flex">
-                      <div className="flex-col justify-start items-start gap-5 flex">
-                        <div className="flex-col justify-start items-start gap-2.5 flex">
-                          <div className="text-black dark:text-neutral-50 text-xl font-medium font-pre">
-                            {inputs.title}
-                          </div>
+                <div className=" border border-gray-200 flex-col justify-center items-center gap-10 inline-flex ">
+                  <div className="flex-col justify-center items-center gap-[50px] flex">
+                    <div className="w-[940px] h-[46px] pr-[795px] pt-[15px] pb-4 border-b border-gray-200 justify-start items-center inline-flex">
+                      <div className="self-stretch pl-[25px] justify-center items-center inline-flex">
+                        <div className="text-black text-xs font-light font-['Inter'] uppercase">
+                          2022-04-05 21:36:16
                         </div>
-                        <div className="flex-col justify-start items-start gap-[50px] flex">
-                          <div className="flex-col justify-start items-start gap-[30px] flex">
-                            <div className="w-[421px] text-neutral-600 dark:text-zinc-100 text-xs font-light font-pre whitespace-pre-wrap">
-                              {inputs.description}
-                            </div>
+                      </div>
+                    </div>
+                    <div className="flex-col justify-center items-center   flex">
+                      <div className="w-[890px] text-black text-sm font-light font-['Pretendard'] uppercase tracking-wide">
+                        <br />
+                        안녕하세요.
+                        <br />
+                        SUMROV입니다.
+                        <br />
+                        <br />
+                        <br />
+                        행정각부의 장은 국무위원 중에서 국무총리의 제청으로
+                        대통령이 임명한다.
+                        <br />
+                        형사피고인은 유죄의 판결이 확정될 때까지는 무죄로
+                        추정된다.
+                        <br />
+                        탄핵결정은 공직으로부터 파면함에 그친다. <br />
+                        그러나, 이에 의하여 민사상이나 형사상의 책임이
+                        면제되지는 아니한다.
+                        <br /> 모든 국민은 학문과 예술의 자유를 가진다. <br />
+                        정부는 회계연도마다 예산안을 편성하여 회계연도 개시
+                        90일전까지 국회에 제출하고,
+                        <br /> 국회는 회계연도 개시 30일전까지 이를 의결하여야
+                        한다. <br />
+                        헌법개정안은 국회가 의결한 후 30일 이내에 국민투표에
+                        붙여 국회의원선거권자 과반수의 투표와 투표자 과반수의
+                        찬성을 얻어야 한다.
+                        <br /> 근로자는 근로조건의 향상을 위하여 자주적인
+                        단결권·단체교섭권 및 단체행동권을 가진다.
+                        <br />
+                        <br />
+                        <br />
+                        누구든지 체포 또는 구속을 당한 때에는 즉시 변호인의
+                        조력을 받을 권리를 가진다. <br />
+                        다만, 형사피고인이 스스로 변호인을 구할 수 없을 때에는
+                        법률이 정하는 바에 의하여 국가가 변호인을 붙인다.
+                        <br /> 국가는 평생교육을 진흥하여야 한다. 국가는 여자의
+                        복지와 권익의 향상을 위하여 노력하여야 한다. <br />
+                        평화통일정책의 수립에 관한 대통령의 자문에 응하기 위하여
+                        민주평화통일자문회의를 둘 수 있다. <br />
+                        대통령·국무총리·국무위원·행정각부의 장·헌법재판소
+                        재판관·법관·중앙선거관리위원회 위원·감사원장·감사위원
+                        기타 법률이 정한 공무원이 그 직무집행에 있어서 헌법이나
+                        법률을 위배한 때에는 국회는 탄핵의 소추를 의결할 수
+                        있다.
+                        <br />
+                      </div>
+                      <br />
+                      <br />
+                      <img
+                        className="w-[680px] h-[680px]"
+                        src="https://via.placeholder.com/680x680"
+                      />
+                      <br />
+                      <br />
+                      <div className="w-[890px] h-[102px] text-black text-sm font-light font-['Pretendard'] uppercase tracking-wide">
+                        누구든지 체포 또는 구속을 당한 때에는 즉시 변호인의
+                        조력을 받을 권리를 가진다. <br />
+                        다만, 형사피고인이 스스로 변호인을 구할 수 없을 때에는
+                        법률이 정하는 바에 의하여 국가가 변호인을 붙인다.
+                        <br /> 국가는 평생교육을 진흥하여야 한다. 국가는 여자의
+                        복지와 권익의 향상을 위하여 노력하여야 한다. <br />
+                        평화통일정책의 수립에 관한 대통령의 자문에 응하기 위하여
+                        민주평화통일자문회의를 둘 수 있다. <br />
+                        대통령·국무총리·국무위원·행정각부의 장·헌법재판소
+                        재판관·법관·중앙선거관리위원회 위원·감사원장·감사위원
+                        기타 법률이 정한 공무원이 그 직무집행에 있어서 헌법이나
+                        법률을 위배한 때에는 국회는 탄핵의 소추를 의결할 수
+                        있다.
+                        <br />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex-col justify-center items-center flex border-t">
+                    <div className="justify-start border items-start inline-flex">
+                      <div
+                        className="h-[46px] pr-[110px] pt-3.5 pb-[15px] bg-zinc-100 border-gray-200 justify-start items-center flex border-r-2
+                      "
+                      >
+                        <div className="self-stretch pl-[25px] justify-center items-center inline-flex">
+                          <div className="text-black text-sm font-light font-['Pretendard'] tracking-wider text-nowrap">
+                            제목
                           </div>
                         </div>
                       </div>
-                      <div
-                        className={cn(
-                          "flex-col justify-center items-center gap-[75px] flex",
-                          yes && "gap-[30px]"
-                        )}
-                      >
-                        {yes && (
-                          <>
-                            <div className="w-[299px] flex mt-3 flex-col">
-                              <div className="flex flex-col">
-                                <div className="text-neutral-600 dark:text-neutral-50 text-[10px] font-normal font-pre">
-                                  Lorem ipsum dolor sit
-                                </div>
-                                <div className="text-neutral-600 dark:text-neutral-50 text-[10px] font-normal font-pre">
-                                  -블랙/S
-                                </div>
-                              </div>
-                              <div className="text-black dark:text-white text-[11px] font-semibold font-pre flex justify-end w-full">
-                                KRW 10,000
-                              </div>
-                              <div className="flex gap-1">
-                                <input
-                                  placeholder="1"
-                                  className="text-[10px] font-normal font-pre w-[37px] h-[18px] pl-2 rounded-sm border border-neutral-300 dark:bg-neutral-900"
-                                />
-
-                                <div className="w-[18px] h-[18px]">
-                                  <Plus className="w-[18px] h-[18px] dark:text-black bg-neutral-300 rounded-sm" />
-                                </div>
-                                <div className="w-[18px] h-[18px]">
-                                  <Minus className="w-[18px] h-[18px] dark:text-black bg-neutral-300 rounded-sm" />
-                                </div>
-                              </div>
-                            </div>
-                            <div className="w-[299px] h-3.5 justify-center items-start gap-[167px] inline-flex">
-                              <div className="dark:text-white text-[11px] font-semibold font-pre">
-                                총상품금액
-                              </div>
-                              <div className="w-[84px] flex justify-center items-center">
-                                <div className="text-blue-500 text-[11px] font-semibold font-pre">
-                                  KRW 10,000
-                                </div>
-                                <div className="text-blue-500 text-[10px] font-normal font-pre">
-                                  (1개)
-                                </div>
-                              </div>
-                            </div>
-                          </>
-                        )}
-                        <div className="flex-col justify-start items-start gap-2.5 flex">
-                          <Button className="rounded-none hover:bg-neutral-900 w-[315px] h-[41px] py-3.5 bg-neutral-900 dark:bg-zinc-600 justify-center items-center inline-flex">
-                            <div className="text-neutral-50 text-[11px] font-medium font-body">
-                              Buy it Now
-                            </div>
-                          </Button>
-                          <div className="w-[315px] h-[41px] py-3.5 border border-neutral-900 dark:border-zinc-600 justify-center items-center inline-flex">
-                            <div className="text-black dark:text-neutral-50 text-[11px] font-medium font-body">
-                              Add to Cart
+                      <div className="w-[841px] h-[46px] pr-[664px] pt-[14.50px] pb-[15.50px] justify-start items-center flex">
+                        <div className="self-stretch pl-[15px] justify-center items-center inline-flex ">
+                          <div className="text-black text-[13px] font-normal font-['Inter'] uppercase ">
+                            2월 BEST REVIEW EVENT !
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="  justify-start border items-start inline-flex w-full">
+                      <div className="pr-[95px] pt-3.5 pb-[15px] bg-zinc-100  justify-start items-center flex border-r-2">
+                        <div className="self-stretch pl-[25px] justify-center items-center inline-flex ">
+                          <div className="text-black text-sm font-light font-['Pretendard'] tracking-wider text-nowrap">
+                            작성자
+                          </div>
+                        </div>
+                      </div>
+                      <div className="w-[841px] h-[46px] pr-[769px] pt-[14.50px] pb-[15.50px] justify-start items-center flex">
+                        <div className="w-[841px] h-[46px] pr-[769px] pt-[14.50px] pb-[15.50px] justify-start items-center inline-flex">
+                          <div className="self-stretch pl-[15px] justify-center items-center inline-flex">
+                            <div className="text-black text-[13px] font-normal font-['Inter'] uppercase">
+                              sumrov
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className="flex-col justify-start items-start gap-10 flex">
-                    <img
-                      className="w-[550px] h-[733.33px]"
-                      src="https://via.placeholder.com/550x733"
-                    />
-                    <img
-                      className="w-[550px] h-[733.33px]"
-                      src="https://via.placeholder.com/550x733"
-                    />
-                    <img
-                      className="w-[550px] h-[733.33px]"
-                      src="https://via.placeholder.com/550x733"
-                    />
-                  </div>
+                </div>
+                <div className="flex-col justify-start items-center gap-[15px] flex">
+                  <div className="justify-center items-center gap-[25px] inline-flex"></div>
                 </div>
               </div>
             </div>
