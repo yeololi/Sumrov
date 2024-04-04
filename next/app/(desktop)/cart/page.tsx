0390799c3411072603a43ed84b75889e7b09f641
dnjeh <ehdnj.001@gmail.com>
2024-04-04 20:44:03 +0900

    checkbox connecting

--- next/app/(desktop)/cart/page.tsx
+++ next/app/(desktop)/cart/page.tsx
@@ -1,3 +1,4 @@
+
 import { Button } from "@/components/ui/button";
 import Footer from "../_components/footer";
 import Header from "../_components/header";
@@ -15,8 +16,8 @@ import {
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/util/authOption";
 import { redirect } from "next/navigation";
-import { useState } from "react";
 import { Row } from "@tanstack/react-table";
+import CartInner from "./_components/cart-inner";
 
 const data: cart[] = [
   {
@@ -40,70 +41,14 @@ const data: cart[] = [
 ];
 
 const Cart = async () => {
-  const session = await getServerSession(authOptions);
-  let total = 0;
-  data.forEach((ai)=>{total += ai.price*ai.amount});
-  const sale=100, box=0;
+  const session = await getServerSession(authOptions) 
 
   console.log(session);
 
   !session && redirect("/login");
 
   return (
-    <>
-      <Header />
-      <div className="flex-col justify-center items-center gap-10 flex pt-[150px] pb-[300px]">
-        <div className="flex-col justify-center items-center gap-6 flex">
-          <div className="text-center text-black text-[26px] font-semibold font-nav tracking-[2.60px]">
-            CART
-          </div>
-          <div className="flex-col justify-center items-center gap-10 flex">
-            <DataTable columns={columns} data={data} />
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="text-center font-noto w-[200px]">
-                    총 상품 금액
-                  </TableHead>
-                  <TableHead className="text-center font-noto w-[200px]">
-                    총 할인 금액
-                  </TableHead>
-                  <TableHead className="text-center font-noto w-[200px]">
-                    총 배송비
-                  </TableHead>
-                  <TableHead className="text-center font-noto w-[400px]">
-                    결제 예정 금액
-                  </TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                <TableRow>
-                  <TableCell className="text-center">KRW {total.toLocaleString()}</TableCell>
-                  <TableCell className="text-center">KRW {sale.toLocaleString()}</TableCell>
-                  <TableCell className="text-center">KRW {box.toLocaleString()}</TableCell>
-                  <TableCell className="text-center">KRW {(total-sale+box).toLocaleString()}</TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </div>
-        </div>
-        <div className="justify-center items-center gap-5 inline-flex">
-          <Button
-            variant={"login"}
-            className="rounded-none w-[425px] h-[50px] py-[9px] bg-neutral-900 justify-center items-center gap-2.5 flex text-base font-semibold font-noto text-center "
-          >
-            모든 항목 주문
-          </Button>
-          <Button
-            variant={"signup"}
-            className="w-[425px] h-[50px] py-[9px] border border-neutral-900 justify-center items-center gap-2.5 flex text-base font-semibold font-noto text-center "
-          >
-            선택 항목 주문
-          </Button>
-        </div>
-      </div>
-      <Footer />
-    </>
+    <CartInner/>
   );
 };
 
