47c9826c3f08693d7b7e1f38dc364907a20ed516
igiza1213 <igiza1213@gmail.com>
2024-04-15 19:29:23 +0900

    c

--- next/app/(desktop)/_components/header.tsx
+++ next/app/(desktop)/_components/header.tsx
@@ -4,7 +4,7 @@ import NavBar from "./navbar";
 
 const Header = () => {
   return (
-    <div className="w-full h-[75px] bg-white bg-opacity-10 dark:bg-opacity-15 px-[100px] flex items-center absolute">
+    <div className="w-full h-[75px] bg-white bg-opacity-10 dark:bg-opacity-5 px-[100px] flex items-center absolute">
       <Link href="/" className="justify-start items-center gap-2.5 inline-flex">
         <img className="w-5 h-5" src="/images/logo.png" />
         <div className="text-black dark:text-white text-lg font-semibold font-nav">
--- next/app/(desktop)/mypage/_components/mypageButton.tsx
+++ next/app/(desktop)/mypage/_components/mypageButton.tsx
@@ -5,35 +5,42 @@ import { signOut } from "next-auth/react";
 import Link from "next/link";
 
 const data = [
-  { name: "주문내역 조회" },
-  { name: "회원정보" },
-  {
-    name: (
-      <>
-        <Link href={"/cart"}>장바구니</Link>
-      </>
-    ),
-  },
-  {
-    name: "로그아웃",
-    onClick: () => {
+  <Button
+    variant={"ghost"}
+    className="text-[12px] font-bold font-pre dark:text-white text-[#000] mb-[40px] min-w-[78px]"
+  >
+    주문내역 조회
+  </Button>,
+  <Button
+    variant={"ghost"}
+    className="text-[12px] font-bold font-pre dark:text-white text-[#000] mb-[40px] min-w-[78px]"
+  >
+    회원정보
+  </Button>,
+  <Button
+    variant={"ghost"}
+    className="text-[12px] font-bold font-pre dark:text-white text-[#000] mb-[40px] min-w-[78px]"
+    asChild
+  >
+    <Link href={"/cart"}>장바구니</Link>
+  </Button>,
+
+  <Button
+    variant={"ghost"}
+    className="text-[12px] font-bold font-pre dark:text-white text-[#000] mb-[40px] min-w-[78px]"
+    onClick={() => {
       signOut({ callbackUrl: "http://localhost:3000/" });
-    },
-  },
+    }}
+  >
+    로그아웃
+  </Button>,
 ];
 
 const MypageButton = () => {
   return (
     <>
       {data.map((arg, key) => (
-        <Button
-          key={key}
-          variant={"ghost"}
-          className="text-[12px] font-bold font-pre dark:text-white text-[#000] mb-[40px] min-w-[78px]"
-          onClick={arg.onClick}
-        >
-          {arg.name}
-        </Button>
+        <div key={key}>{arg}</div>
       ))}
     </>
   );
