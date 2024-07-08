fb307a1bb052a3306b0e9e49f711550e4cb36e0c
igiza1213 <igiza1213@gmail.com>
2024-07-08 23:30:53 +0900

    Update route.ts

--- next/app/api/auth/login/route.ts
+++ next/app/api/auth/login/route.ts
@@ -9,17 +9,29 @@ interface RequestBody {
 
 export async function POST(request: Request) {
   const body: RequestBody = await request.json();
-  const db = (await clientPromise).db("sumrov");
-  const user = await db.collection("users").findOne({ email: body.email });
-  if (user && (await bcrypt.compare(body.password, user.password))) {
-    const { password, ...userWithoutPass } = user;
 
-    const accessToken = signJwtAccessToken(userWithoutPass);
-    const result = {
-      ...userWithoutPass,
-      accessToken,
-    };
+  try {
+    const db = (await clientPromise).db("sumrov");
+    const user = await db.collection("users").findOne({ email: body.email });
 
-    return new Response(JSON.stringify(result));
-  } else return new Response(JSON.stringify(null));
+    if (user && (await bcrypt.compare(body.password, user.password))) {
+      const { password, ...userWithoutPass } = user;
+
+      const accessToken = signJwtAccessToken(userWithoutPass);
+      const result = {
+        ...userWithoutPass,
+        accessToken,
+      };
+
+      return new Response(JSON.stringify(result));
+    } else
+      return new Response(JSON.stringify(null), {
+        status: 401,
+      });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify(null), {
+      status: 401,
+    });
+  }
 }
