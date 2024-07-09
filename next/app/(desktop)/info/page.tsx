499a027d3465958477047d715ad5e6b5fe1aff5f
igiza1213 <igiza1213@gmail.com>
2024-07-09 23:29:42 +0900

    i hate eslint

--- next/app/(desktop)/info/page.tsx
+++ next/app/(desktop)/info/page.tsx
@@ -8,7 +8,7 @@ export default function Info() {
       <div className="relative">
         <img src="/images/main1.jpg" alt="" className=" w-screen pt-[50px]" />
         <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 font-pre text-center dark:text-black">
-          <h1 className="font-bold text-3xl">&quot;It's ok&quot;</h1>
+          <h1 className="font-bold text-3xl">&quot;It&apos;s ok&quot;</h1>
           <p className="text-xl">
             <br />
             안녕하세요 SUMROV입니다.
@@ -16,7 +16,8 @@ export default function Info() {
             여러분의 하루는 괜찮았나요?
             <br />
             <br />
-            SUMROV는 스스로에게 "괜찮아" 라는 말을 대신 전해주는 브랜드입니다.
+            SUMROV는 스스로에게 &quot;괜찮아&quot; 라는 말을 대신 전해주는
+            브랜드입니다.
             <br />
             괜찮지 않았다면 우리 잠시 쉬었다 가요. 당신의 하루에 생각나도록
             <br />
