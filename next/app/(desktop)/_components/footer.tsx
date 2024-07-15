e2de101fbdeb4f0e866299c1cc6f8e7b9bf46a89
igiza1213 <igiza1213@gmail.com>
2024-07-15 22:48:41 +0900

    Update footer.tsx

--- next/app/(desktop)/_components/footer.tsx
+++ next/app/(desktop)/_components/footer.tsx
@@ -63,7 +63,7 @@ const Footer = () => {
               썸로브(sumrov) CEO : JAEHEE OH 사업자번호 : 626-62-00566
               통신판매신고번호 : 2023-전북군산-0504 호 (사업자 정보 확인)
               <br />
-              이메일 문의 : ojh@sumrov.com 이메일 문의 : sumrov._
+              이메일 문의 : ojh@sumrov.com 인스타 문의 : sumrov._
               개인정보보호책임자 : 오재희
             </div>
           </div>
