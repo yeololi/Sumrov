94216f3a69fa98dfd6226fef5c9ed37d9dbb8fb0
igiza1213 <igiza1213@gmail.com>
2024-07-31 16:06:14 +0900

    Update header.tsx

--- next/app/(mobile)/mobile/_components/header.tsx
+++ next/app/(mobile)/mobile/_components/header.tsx
@@ -11,7 +11,6 @@ const Header = () => {
           <Menu className="w-[20px] h-[20px] dark:text-white" />
         </Link>
         <Link href={"/mobile"} className="flex items-center">
-          <img className="w-[13px] h-[13px] mr-1" src="/images/logo.png" />
           <div className="dark:text-white text-black text-[15px] font-semibold font-nav">
             SUMROV
           </div>
