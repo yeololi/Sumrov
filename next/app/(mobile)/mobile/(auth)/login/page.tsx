bb1124ee7b2a6f6feb84bc98b25e0380afc12591
정삼복 <68502584+3boku@users.noreply.github.com>
2024-10-26 10:50:02 +0900

    모바일 카카오 버튼 추가

--- next/app/(mobile)/mobile/(auth)/login/page.tsx
+++ next/app/(mobile)/mobile/(auth)/login/page.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 import Header from "../../_components/header";
 import LoginForm from "./_components/loginForm";
+import Image from "next/image";
 
 const Login = () => {
     return (
         <>
             <Header />
             <div className="flex flex-col items-center pt-[139px]">
+                <LoginForm />
                 <div className="w-[336px] h-[356px] flex-col justify-start items-center gap-[19px] inline-flex">
-                    <LoginForm />
                     <div className="justify-between items-center flex w-full">
                         <Link
                             href={"/mobile/findEmail"}
